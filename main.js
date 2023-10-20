b20890171b30d55e23081bd188fe33c8e670e891
chocobar <chocobardiazjulieta13@gmail.com>
2023-10-19 21:55:57 -0300

    haciendo correcciones de la primer entrega

--- main.js
+++ main.js
@@ -5,72 +5,86 @@ const productos = [
     { nombre: "gel limpiador", precio: 6800 },
     { nombre: "agua micelar", precio: 3000 },
 ];
-let carrito = [];
+
+const carrito = [];
 let total = 0;
 
-let seleccion = prompt("Hola, ¿desea comprar algún producto? (si o no)");
-
-while (seleccion !== "si" && seleccion !== "no") {
-    alert("Por favor, ingrese 'si' o 'no'.");
-    seleccion = prompt("Hola, ¿desea comprar algo? (si o no)");
+function mostrarProductosDisponibles() {
+    let productosDisponibles = "Productos disponibles:\n";
+    productos.forEach((producto, index) => {
+        productosDisponibles += `${index + 1}. ${producto.nombre} - ${producto.precio}$\n`;
+    });
+    alert(productosDisponibles);
 }
 
-if (seleccion === "si") {
-    alert("A continuación, nuestra lista de productos:");
-    let todosLosProductos = productos.map(
-        (producto) => `${producto.nombre} - ${producto.precio}$`
-    );
-    alert(todosLosProductos.join("-"));
-} else if (seleccion === "no") {
-    alert("¡Gracias por venir! ¡Hasta pronto!");
+
+
+function agregarProductoAlCarrito(producto, unidades) {
+    const productoEncontrado = productos.find(p => p.nombre === producto);
+
+    if (productoEncontrado) {
+        const { nombre, precio } = productoEncontrado;
+        carrito.push({ nombre, unidades, precio });
+        total += unidades * precio;
+        console.log(`Producto: ${nombre}, Unidades: ${unidades}, Total a pagar: ${unidades * precio}`);
+    } else {
+        console.log("El producto seleccionado no está disponible.");
+    }
 }
 
-while (seleccion !== "no") {
-    let producto = prompt("Agregue un producto a su carrito");
-    let precio = 0;
-    let encontrado = false;
+function realizarCompra() {
+    if (carrito.length > 0) {
+        console.log("Gracias por la compra. Resumen de compra:");
+        const productosEnCarrito = {};
 
-    for (let i = 0; i < productos.length; i++) {
-        if (productos[i].nombre === producto) {
-            encontrado = true;
-            precio = productos[i].precio;
-            break;
+        carrito.forEach(item => {
+            const { nombre, unidades, precio } = item;
+
+            if (!productosEnCarrito[nombre]) {
+                productosEnCarrito[nombre] = { unidades: 0, precio };
+            }
+
+            productosEnCarrito[nombre].unidades += unidades;
+        });
+
+        for (const nombreProducto in productosEnCarrito) {
+            if (productosEnCarrito.hasOwnProperty(nombreProducto)) {
+                const { unidades, precio } = productosEnCarrito[nombreProducto];
+                console.log(`Producto: ${nombreProducto}, Unidades: ${unidades}, Total a pagar: ${unidades * precio}`);
+            }
+        }
+
+        console.log(`El total a pagar por sus compras es: ${total}`);
+    } else {
+        console.log("No se agregaron productos al carrito. ¡Hasta pronto!");
+    }
+}
+
+function iniciarCompra() {
+    let continuarComprando = true;
+
+    while (continuarComprando) {
+        mostrarProductosDisponibles();
+        let producto = prompt("Agregue un producto a su carrito");
+        let unidades = parseInt(prompt("¿Cuántas unidades quiere llevar?"));
+        agregarProductoAlCarrito(producto, unidades);
+
+        let respuesta = prompt("¿Desea seguir comprando? (si o no)");
+        if (respuesta.toLowerCase() !== "si") {
+            continuarComprando = false;
         }
     }
 
-    if (encontrado) {
-        let unidades = parseInt(prompt("¿Cuántas unidades quiere llevar?"));
-        carrito.push({ producto, unidades, precio });
-        total += unidades * precio; 
-        console.log(carrito);
+    realizarCompra();
+}
+
+function iniciarPrograma() {
+    let seleccion = prompt("Hola, ¿desea comprar algún producto? (si o no)");
+    if (seleccion.toLowerCase() === "si") {
+        iniciarCompra();
     } else {
-        alert("Producto no disponible.");
+        console.log("¡Gracias por visitarnos! ¡Hasta pronto!");
     }
-
-    seleccion = prompt("¿Desea seguir comprando? (si o no)");
 }
 
-if (seleccion === "no" && carrito.length > 0) {
-    alert("Gracias por la compra. Aquí está su resumen de compra:");
-
-    for (let i = 0; i < carrito.length; i++) {
-        let item = carrito[i];
-        console.log(`Producto: ${item.producto}, Unidades: ${item.unidades}, Total a pagar: ${item.unidades * item.precio}`);
-    }
-
-    console.log(`El total a pagar por sus compras es: ${total}`);
-} else {
-    alert("Gracias por visitarnos. ¡Hasta pronto!");
-}
-
-
-
-
-
-
-
-
-
-
-
-
+iniciarPrograma();
