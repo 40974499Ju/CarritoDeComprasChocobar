5aab8a4ea4b63a502421614b3a717349a418b82f
chocobar <chocobardiazjulieta13@gmail.com>
2023-11-21 23:35:29 -0300

    entrega

--- main.js
+++ main.js
@@ -1,77 +1,127 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const cartItems = [];
+const btnCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector(
+	'.container-cart-products'
+);
 
-    function updateCart() {
-        const cartContainer = document.querySelector('.buy-card ul.nav-card');
-        cartContainer.innerHTML = ''; // Limpiar contenido actual
-
-        let total = 0;
-
-        cartItems.forEach(item => {
-            const cartItem = document.createElement('li');
-            cartItem.innerHTML = `
-            <div class="lista_de_biografias">
-                <img src="${item.image}" alt="${item.name}">
-                <p>${item.name}</p>
-                <p>Precio: $${item.price}</p>
-                <p>Cantidad: ${item.quantity}</p>
-                <p><button class="remove-item" data-id="${item.id}">Eliminar</button></p>
-            </div>
-            `;
-
-            cartContainer.appendChild(cartItem);
-            total += item.price * item.quantity;
-        });
-
-        // Mostrar total
-        const totalElement = document.createElement('li');
-        totalElement.textContent = `Total: $${total}`;
-        cartContainer.appendChild(totalElement);
-    }
-
-    function addToCart(button) {
-        const itemId = parseInt(button.getAttribute('data-id'));
-        const itemName = button.getAttribute('data-title');
-        const itemPrice = parseFloat(button.getAttribute('data-price'));
-        const itemImage = button.getAttribute('data-image');
-
-        const existingItem = cartItems.find(item => item.id === itemId);
-
-        if (existingItem) {
-            existingItem.quantity += 1;
-        } else {
-            cartItems.push({ id: itemId, name: itemName, price: itemPrice, quantity: 1, image: itemImage });
-        }
-
-        updateCart();
-    }
-
-    function removeFromCart(id) {
-        const index = cartItems.findIndex(item => item.id === id);
-
-        if (index !== -1) {
-            if (cartItems[index].quantity > 1) {
-                cartItems[index].quantity -= 1;
-            } else {
-                cartItems.splice(index, 1);
-            }
-            updateCart();
-        }
-    }
-
-    function clearCart() {
-        cartItems.length = 0;
-        updateCart();
-    }
-
-    document.addEventListener('click', function (event) {
-        if (event.target.classList.contains('add-to-cart')) {
-            addToCart(event.target);
-        } else if (event.target.classList.contains('remove-item')) {
-            const itemId = parseInt(event.target.getAttribute('data-id'));
-            removeFromCart(itemId);
-        } else if (event.target.classList.contains('clear-cart')) {
-            clearCart();
-        }
-    });
+btnCart.addEventListener('click', () => {
+	containerCartProducts.classList.toggle('hidden-cart');
 });
+
+/* ========================= */
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
+
+// Lista de todos los contenedores de productos
+const productsList = document.querySelector('.container-items');
+
+// Variable de arreglos de Productos
+let allProducts = [];
+
+const valorTotal = document.querySelector('.total-pagar');
+
+const countProducts = document.querySelector('#contador-productos');
+
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
+
+productsList.addEventListener('click', e => {
+	if (e.target.classList.contains('btn-add-cart')) {
+		const product = e.target.parentElement;
+
+		const infoProduct = {
+			quantity: 1,
+			title: product.querySelector('h2').textContent,
+			price: product.querySelector('p').textContent,
+		};
+
+		const exits = allProducts.some(
+			product => product.title === infoProduct.title
+		);
+
+		if (exits) {
+			const products = allProducts.map(product => {
+				if (product.title === infoProduct.title) {
+					product.quantity++;
+					return product;
+				} else {
+					return product;
+				}
+			});
+			allProducts = [...products];
+		} else {
+			allProducts = [...allProducts, infoProduct];
+		}
+
+		showHTML();
+	}
+});
+
+rowProduct.addEventListener('click', e => {
+	if (e.target.classList.contains('icon-close')) {
+		const product = e.target.parentElement;
+		const title = product.querySelector('p').textContent;
+
+		allProducts = allProducts.filter(
+			product => product.title !== title
+		);
+
+		console.log(allProducts);
+
+		showHTML();
+	}
+});
+
+// Funcion para mostrar  HTML
+const showHTML = () => {
+	if (!allProducts.length) {
+		cartEmpty.classList.remove('hidden');
+		rowProduct.classList.add('hidden');
+		cartTotal.classList.add('hidden');
+	} else {
+		cartEmpty.classList.add('hidden');
+		rowProduct.classList.remove('hidden');
+		cartTotal.classList.remove('hidden');
+	}
+
+	// Limpiar HTML
+	rowProduct.innerHTML = '';
+
+	let total = 0;
+	let totalOfProducts = 0;
+
+	allProducts.forEach(product => {
+		const containerProduct = document.createElement('div');
+		containerProduct.classList.add('cart-product');
+
+		containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+                <span class="precio-producto-carrito">${product.price}</span>
+            </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        `;
+
+		rowProduct.append(containerProduct);
+
+		total =
+			total + parseInt(product.quantity * product.price.slice(1));
+		totalOfProducts = totalOfProducts + product.quantity;
+	});
+
+	valorTotal.innerText = `$${total}`;
+	countProducts.innerText = totalOfProducts;
+};
